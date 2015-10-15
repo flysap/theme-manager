eaed68a997bf270952f79595a46ea9840cd2dc7c
Radu <radu.bordei89@gmail.com>
2015-10-15 11:16:30 +0300

    extends layout.

--- views/upload.blade.php
+++ views/upload.blade.php
@@ -1,6 +1,10 @@
-<form enctype="multipart/form-data" method="post" action="{{route('theme-upload')}}">
+@extends('themes::layouts.default')
+
+@section('content')
+    <form enctype="multipart/form-data" method="post" action="{{route('theme-upload')}}">
     <input type="file" name="theme">
 
     <input type="hidden" name="_token" value="{{csrf_token()}}">
     <input type="submit" value="Upload">
 </form>
+@endsection
